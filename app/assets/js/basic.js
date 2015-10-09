a06730970a039f9ae75ae8490fcf30d57afb40b2
bverjat <benoit.verjat@sciences-po.fr>
2015-10-09 15:44:34 +0200

    adding slider and new tag from heurist

--- app/assets/js/basic.js
+++ app/assets/js/basic.js
@@ -1,9 +1,11 @@
 // const apiUrl = 'data/heurist-cache-2.json';
+
 console.log(apiUrl)
+
+
 d3.json(apiUrl, function(error, data) {
 
   if (error) throw error;
-
 
   console.log(data);
 
@@ -32,49 +34,47 @@ d3.json(apiUrl, function(error, data) {
     .value();
 
   // doc index
-  graph.doc_index = {}
+  graph.docIndex = {}
   graph.doc.forEach(function(d){
-    graph.doc_index[d.recordId] = d
+    graph.docIndex[d.recordId] = d
   })
 
   // org index
-  graph.org_index = {}
+  graph.orgIndex = {}
   graph.org.forEach(function(d){
-    graph.org_index[d.recordId] = d
+    graph.orgIndex[d.recordId] = d
   })
 
   // rel index
-  graph.rel_index = {}
+  graph.relIndex = {}
   graph.rel.forEach(function(d){
-    graph.rel_index[d.recordId] = d
+    graph.relIndex[d.recordId] = d
   })
 
   // matching tables
-  var obj_to_y = {};
+  var eventYpos = {};
 
-  var state_offset = 80,
-      state_spacing = 11
+  var stateOffset = 80,
+      stateSpacing = 11
 
   graph.sta.forEach(function(d, i){
-    obj_to_y[d.recordId] = state_offset + i * state_spacing
+    eventYpos[d.recordId] = stateOffset + i * stateSpacing
   })
 
 
-  var doc_offset = state_offset + state_spacing*(graph.sta.length+1),
-      doc_spacing = 11
+  var docOffset = stateOffset + stateSpacing*(graph.sta.length+1),
+      docSpacing = 11
 
   graph.doc.forEach(function(d, i){
-    obj_to_y[d.recordId] = doc_offset + i * doc_spacing
+    eventYpos[d.recordId] = docOffset + i * docSpacing
   })
 
   var org_spacing = 11,
-      org_offset = doc_offset + org_spacing*(graph.doc.length+1)
+      orgOffset = docOffset + org_spacing*(graph.doc.length+1)
 
   graph.org.forEach(function(d, i){
-    obj_to_y[d.recordId] = org_offset + i * org_spacing
+    eventYpos[d.recordId] = orgOffset + i * org_spacing
   })
-
-
 
   console.log(graph);
 
@@ -82,7 +82,8 @@ d3.json(apiUrl, function(error, data) {
     color = d3.scale.category20();
     start = so.getTimeBounds().start,
     end = so.getTimeBounds().end,
-    rel_offset = 250, rel_spacing = 6,
+    rel_offset = 250, relSpacing = 6,
+
 
   svg = d3.select('#basic')
     .append('svg:svg')
@@ -91,104 +92,149 @@ d3.json(apiUrl, function(error, data) {
 
   var relTypes = so.getTypes({'recordTypeId':1});
 
-  // draw organisations, document and state list
-  var list = svg.selectAll('.org')
-    .data(graph.org.slice(0).concat(graph.doc).slice(0).concat(graph.sta))
-    .enter()
-    .append('g')
-    .attr('class','listItem')
 
-  list.append('text')
-    .attr('x', 20)
-    .attr('y', function(d){return obj_to_y[d.recordId]})
-    .attr('transform', 'translate(0, 4)')
-    .text(function(d){return d.shortName})
 
-  list.append('line')
-    .attr('x1', 100)
-    .attr('y1', function(d){return obj_to_y[d.recordId]})
-    .attr('x2', w)
-    .attr('y2', function(d){return obj_to_y[d.recordId]})
-    .attr('class','axis' )
-    .style('stroke', 'grey')
-    .attr('transform', 'translate(-80)')
-    .attr('id',function(d){ return 'l'+d.recordId } )
-    .on('mouseover', function(d) {d3.select(this).style('stroke', 'red');})
-    .on('mouseout', function(d) {d3.select(this).style('stroke', 'grey');});
-
-  // draw events
-  var prevDate,event = svg.selectAll('.event')
-    .data(graph.rel)
-    .enter()
-    .append('g')
-    .attr('id',function(d){ return d.recordId } )
-    .attr('class', function(d){
-
-      var isShown = prevDate!==d.startDate;
-      prevDate = d.startDate;
-
-      return isShown ? 'event newYear':'event';
-    })
-    ;
-
-  event.append('title')
-    .text(function(d) {return  d.source.shortName + ' ' + d.typeName + ' ' + d.target.shortName+' ('+d.recordId+')\n—\n[[' + d.title + ']]'})
-
-  event.append('text')
-    .attr('x', function(d,i){ return rel_offset + i * rel_spacing })
-    .attr('y', function(d,i){ return 10+ (d.startDate%6) * 10})
-    .text(function(d){return d.startDate})
-    .attr('class', 'yearLabel')
-    .attr('text-anchor', 'middle');
-
-  event.append('line')
-    .attr('x1', function(d,i){ return rel_offset + i * rel_spacing })
-    .attr('x2', function(d,i){ return rel_offset + i * rel_spacing })
-    .attr('y1', function(d,i){ return 15+ (d.startDate%6) * 10})
-    .attr('y2', h)
-    .attr('class', 'yearMark')
-    .style('stroke-width', 1)
-    .style('stroke', 'black')
-
-  event.append('line')
-    .attr('x1', function(d,i){ return rel_offset + i * rel_spacing })
-    .attr('x2', function(d,i){ return rel_offset + i * rel_spacing })
-    .attr('y1', function(d){ return obj_to_y[d.source.recordId] })
-    .attr('y2', function(d){ return obj_to_y[d.target.recordId] })
-    .style('stroke',function(d){return color(_.indexOf(relTypes, d.typeId));})
-    .style('stroke-width', 1)
-
-  event.append('circle')
-    .attr('cx', function(d,i){ return rel_offset + i * rel_spacing })
-    .attr('cy', function(d){ return obj_to_y[d.source.recordId] })
-    .attr('r', 4)
-    .style('fill',function(d){return color(_.indexOf(relTypes, d.typeId));})
-    .on('mouseover', function(d) {
+  function focusOn(d){
+    d3.select(this).style('stroke', 'red');
+    if(d.source){
       d3.select('#l'+d.target.recordId).style('stroke', 'red');
       d3.select('#l'+d.source.recordId).style('stroke', 'red');
-    })
-    .on('mouseout', function(d) {
+    }
+  }
+  function focusOff(d){
+    d3.select(this).style('stroke', 'grey');
+    if(d.target){
       d3.select('#l'+d.target.recordId).style('stroke', 'grey');
       d3.select('#l'+d.source.recordId).style('stroke', 'grey');
-    });
+    }
+  }
 
-  event.append('circle')
-    .attr('cx', function(d,i){ return rel_offset + i * rel_spacing })
-    .attr('cy', function(d){return obj_to_y[d.target.recordId] })
-    .attr('r', 4)
-    .attr('title', function(d){ d.shortName } )
-    .style('fill', function(d){return color(_.indexOf(relTypes, d.typeId));})
-    .on('mouseover', function(d) {
-      d3.select('#l'+d.target.recordId).style('stroke', 'red');
-      d3.select('#l'+d.source.recordId).style('stroke', 'red');
-    })
-    .on('mouseout', function(d) {
-      d3.select('#l'+d.target.recordId).style('stroke', 'grey');
-      d3.select('#l'+d.source.recordId).style('stroke', 'grey');
-    });
+  function sourceY(d){ return eventYpos[d.source.recordId] }
+  function targetY(d){ return eventYpos[d.target.recordId] }
+  function relX(d,i){  return rel_offset + i * relSpacing }
+  function relTypeColor(d){return color(_.indexOf(relTypes, d.typeId));}
 
+  function create(){
+    // draw organisations, document and state list
+    var list = svg.selectAll('.org')
+      .data(graph.org.slice(0).concat(graph.doc).slice(0).concat(graph.sta))
+      .enter()
+      .append('g')
+      .attr('class','listItem')
+
+    list.append('text')
+      .attr('x', 20)
+      .attr('y', function(d){return eventYpos[d.recordId]})
+      .attr('transform', 'translate(0, 4)')
+      .text(function(d){return d.shortName})
+
+    list.append('line')
+      .attr('x1', 100)
+      .attr('y1', function(d){return eventYpos[d.recordId]})
+      .attr('x2', w)
+      .attr('y2', function(d){return eventYpos[d.recordId]})
+      .attr('class','axis' )
+      .style('stroke', 'grey')
+      .attr('transform', 'translate(-80)')
+      .attr('id',function(d){ return 'l'+d.recordId } )
+      .on('mouseover', focusOn)
+      .on('mouseout', focusOff);
+
+    // draw events
+    var prevDate;
+
+    var event = svg.selectAll('.event')
+      .data(graph.rel)
+      .enter()
+      .append('g')
+      .attr('id',function(d){ return d.recordId } )
+      .attr('class', function(d){
+
+        var isShown = prevDate!==d.startDate;
+        prevDate = d.startDate;
+
+        return isShown ? 'event newYear':'event';
+      })
+      ;
+
+    // event title info
+    event.append('title')
+      .text(function(d) {return  d.source.shortName + ' ' + d.typeName + ' ' + d.target.shortName+' ('+d.recordId+')\n—\n[[' + d.title + ']]'})
+
+    // year label
+    event.append('text')
+      .attr('class', 'yearLabel')
+      .text(function(d){return d.startDate})
+      .attr('text-anchor', 'middle');
+
+    // year mark
+    event.append('line')
+      .attr('class', 'yearMark')
+      .attr('y1', function(d,i){ return 15+ (d.startDate%6) * 10})
+      .attr('y2', h)
+
+    // edges
+    event.append('line')
+      .attr('class', 'edges')
+      .style('stroke',relTypeColor)
+      .attr('y1', sourceY)
+      .attr('y2', targetY)
+
+    // source node
+    event.append('circle')
+      .attr('class','node source' )
+      .attr('cy', sourceY)
+      .style('fill',relTypeColor)
+      .attr('r', 4)
+      .on('mouseover', focusOn)
+      .on('mouseout', focusOff);
+
+    // target node
+    event.append('circle')
+      .attr('class','node target')
+      .attr('cy', targetY)
+      .style('fill', relTypeColor)
+      .attr('r', 4)
+      .on('mouseover', focusOn)
+      .on('mouseout', focusOff);
+
+    update();
+  }
+
+  function update(){
+
+    // year label
+    d3.selectAll('.yearLabel')
+      .attr('x',relX)
+      .attr('y', function(d,i){ return 10+ (d.startDate%6) * 10})
+
+    // year mark
+    d3.selectAll('.yearMark')
+      .attr('x1', relX)
+      .attr('x2', relX)
+
+    // edges
+    d3.selectAll('.edges')
+      .attr('x1', relX)
+      .attr('x2', relX)
+
+    // source node
+    d3.selectAll('.source.node')
+      .attr('cx', relX)
+
+    // target node
+    d3.selectAll('.target.node')
+      .attr('cx', relX)
+
+  }
+
+  d3.select("#zoom").on("input", function() {
+    relSpacing = this.value;
+    update();
+  });
+
+
+  create();
 })
-
-
 
 
